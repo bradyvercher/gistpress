03fde4d15f79382c45e373d96a3dc15c147279ff
GaryJones <gary@garyjones.co.uk>
2012-12-30 18:10:53 +0000

    Refactor main plugin file.
    The instantiation of main and logging class is kept in the global scope, but the injection of the logging class into the main class, and the calling of the main class run() method that hooks in all of the other actions and filters, is now done within a function hooked to the init action.
    
    The loading of the plugin text domain / localization is also improved, and hooked into the init action as well (but in a different function).

--- blazer-six-gist-oembed.php
+++ blazer-six-gist-oembed.php
@@ -16,20 +16,46 @@
  * @license GPL-2.0+
  */
 
-// Support localization
-load_plugin_textdomain( 'blazersix-gist-oembed', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
-
-// Set up main plugin and logging classes
+// Instantiate main plugin class
 if ( ! class_exists( 'Blazer_Six_Gist_oEmbed' ) )
 	require( plugin_dir_path( __FILE__ ) . 'class-blazer-six-gist-oembed.php' );
+$gist_oembed = new Blazer_Six_Gist_oEmbed;
 
+// Instantiate logging class
 if ( ! class_exists( 'Blazer_Six_Gist_oEmbed_Log' ) )
 	require( plugin_dir_path( __FILE__ ) . 'class-blazer-six-gist-oembed-log.php' );
-
-$gist_oembed = new Blazer_Six_Gist_oEmbed;
 $gist_oembed_logger = new Blazer_Six_Gist_oEmbed_Log;
-$gist_oembed->set_logger( $gist_oembed_logger );
-$gist_oembed->run();
+
+add_action( 'init', 'blazer_six_gist_oembed_localization' );
+/**
+ * Support localization for plugin.
+ *
+ * @see http://www.geertdedeckere.be/article/loading-wordpress-language-files-the-right-way
+ *
+ * @since 1.1.0
+ */
+function blazer_six_gist_oembed_localization() {
+	$domain = 'blazersix-gist-oembed';
+	// The "plugin_locale" filter is also used in load_plugin_textdomain()
+	$locale = apply_filters( 'plugin_locale', get_locale(), $domain );
+	load_textdomain( $domain, WP_LANG_DIR . '/my-plugin/' . $domain . '-' . $locale . '.mo' );
+	load_plugin_textdomain( $domain, false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+}
+
+add_action( 'init', 'blazer_six_gist_oembed_init' );
+/**
+ * Set plugin logger class and initialise plugin.
+ *
+ * If you want a different logger class, then unhook this function, and hook
+ * in your own which does what you need.
+ *
+ * @since 1.1.0
+ */
+function blazer_six_gist_oembed_init() {
+	global $gist_oembed, $gist_oembed_logger;
+	$gist_oembed->set_logger( $gist_oembed_logger );
+	$gist_oembed->run();
+}
 
 /** @todo Can the following two functions be made static methods of the debug
  * bar class, since there is already an inherent dependency? */
