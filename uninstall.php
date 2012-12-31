07655d9bddfbd552bbdbcae38bee4933f98dba1d
Brady Vercher <brady@blazersix.com>
2012-12-30 21:12:19 -0600

    Updated the uninstall file with new key names and to use the API to delete cache entries so filters hooked into them will fire.

--- uninstall.php
+++ uninstall.php
@@ -5,6 +5,20 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 
 global $wpdb;
 
-$wpdb->query( "DELETE FROM $wpdb->postmeta WHERE meta_key LIKE '_gist_embed_%'" );
-$wpdb->query( "DELETE FROM $wpdb->options WHERE option_name LIKE '_transient_gist_embed_%' OR option_name LIKE '_transient_timeout_gist_embed_%" );
 delete_option( 'blazersix_gist_embed_stylesheet' );
+
+// Delete post meta.
+$post_metas = $wpdb->get_results( "SELECT post_id, meta_key FROM $wpdb->postmeta WHERE meta_key LIKE '_gist_raw_%'" );
+if ( $post_metas ) {
+	foreach( $post_metas as $meta ) {
+		delete_post_meta( $meta->post_id, $meta->meta_key );
+	}
+}
+
+// Delete transients.
+$transients = $wpdb->get_col( "SELECT option_name FROM $wpdb->options WHERE option_name LIKE '_transient_gist_html_%' OR option_name LIKE '_transient__gist_raw_%'" );
+if ( $transients ) {
+	foreach ( $transients as $key ) {
+		delete_transient( $key );
+	}
+}
