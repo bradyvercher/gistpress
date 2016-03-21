453dbc0f08a3e82e7ad67257de1baa9257f5be65
Gary Jones <gary@gamajo.com>
2016-03-21 17:27:33 +0000

    Tests: Add start of WP unit tests

---
+++ tests/test-sample.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package gistpress
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
+
